3405d62465cba7707be1d4ebb0a9753140ad2d1e
Vitor <vitorguima@gmail.com>
2022-03-23 19:45:51 -0300

    feat: implement logic for posts controller to accept post requests containing a json with title and content params

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,12 +4,18 @@ class PostsController < ApplicationController
   end
 
   def create_post
-    post = Post.new(title: 'test_method', content: 'test_curl')
+    post = Post.new(post_params)
 
     if post.save
       render json: post, status: :created
     else
       render json: book.errors, status: :unproccessable_entity
     end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :content)
   end
 end
