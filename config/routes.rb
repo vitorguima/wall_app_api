1ad8f69654970530313e4c55b0e7d36c10fff10b
Vitor <vitorguima@gmail.com>
2022-03-27 23:20:59 -0300

    feat: delete user and put post routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,10 @@ Rails.application.routes.draw do
       get '/posts' => 'posts#get_posts_list'
       post '/posts' => 'posts#create_post'
       delete '/posts/:id' => 'posts#delete_post'
+      put '/posts/:id' => 'posts#update_post'
 
       post '/user' => 'user#create_user'
+      delete '/user' => 'user#delete_user'
 
       post '/authenticate' => 'authentication#create'
     end
