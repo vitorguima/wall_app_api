9b117396890fff74220d6374bc0a01a1f1bc47cb
Vitor <vitorguima@gmail.com>
2022-03-30 19:56:52 -0300

    feat: implement email format validation for register new user endpoint

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,20 @@ class User < ApplicationRecord
 
   validates :first_name, presence: true
   validates :last_name, presence: true
-  validates :nickname, presence: true
+  validates :nickname, presence: true, uniqueness: true
   validates :email, presence: true, uniqueness: true
+  validate :email_format
+
+  private
+
+  def email_format
+    pattern = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+    email = self.email
+    
+    if pattern =~ email
+      return
+    else
+      self.errors.add(:email, 'Invalid email format')
+    end
+  end
 end
