68061ca449f54e53aa5b187a063ac2f15322cb5c
Vitor <vitorguima@gmail.com>
2022-04-02 20:12:15 -0300

    refactor: change token and user_id methods to AppController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,20 @@
 class ApplicationController < ActionController::API
   rescue_from ActiveRecord::RecordNotDestroyed, with: :not_destroyed
 
+  include ActionController::HttpAuthentication::Token
+
   private
 
   def not_destroyed(error)
     render json: { errors: e.record.errors }, status: :unprocessable_entity
+  end
+
+  def token
+    token, _options = token_and_options(request)
+    @token ||= token
+  end
+
+  def user_id
+    @user_id ||= AuthenticationTokenService.decode(token) unless token.nil?
   end
 end
